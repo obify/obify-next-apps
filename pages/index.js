eb6b54bc31de572820a67ed8d985a2b8acc87571
Ranjan <skillsapphire@gmail.com>
2023-04-01 20:06:48 +0200

    more links

--- pages/index.js
+++ pages/index.js
@@ -16,8 +16,10 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <h2>OBIFY CONSULTING</h2><br />
-        <a href='https://www.obifyconsulting.com'>obifyconsulting apps</a>
-        <a style={{ display: 'none' }} href='https://github.com/obify/obify-next-apps.git'>obify-next-apps.git</a><br />
+        <a target='_blank' href='https://stellular-duckanoo-b92d72.netlify.app/'>HTML & CSS Practicals - 1</a>
+        <a style={{ display: 'none' }} href='https://github.com/ranjanpandeysbp/training-fe'>training-fe</a><br />
+        <a target='_blank' href='https://verdant-zabaione-915e20.netlify.app/'>HTML & CSS Practicals - 2</a>
+        <a style={{ display: 'none' }} href='https://github.com/mahesh1404/Frontend'>Frontend</a><br />
       </main>
     </>
   )
