d4b7a858dde1947ed4d77fe37e8f957a7de320c3
Ranjan <skillsapphire@gmail.com>
2023-04-01 21:33:50 +0200

    product fake api

--- pages/api/products.js
+++ pages/api/products.js
@@ -3,7 +3,7 @@ import { promises as fs } from 'fs';
 export default async function handler(req, res) {
     //Find the absolute path of the json directory
     //Read the json data file data.json
-    const fileContents = await fs.readFile('./productsdata.json', 'utf8');
+    const fileContents = await fs.readFile('/productsdata.json', 'utf8');
     //Return the content of the data file in json format
     res.status(200).json({ list: JSON.parse(fileContents) });
 }
